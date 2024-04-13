0cc0dc19982cf99d0348532607ffa58774acefe1
Koliras <gyrmanchyk230506@gmail.com>
2024-04-13 20:28:21 +0300

    improve error handling, split code

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,45 @@
-use std::{env, fs};
+use std::{env, error::Error, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let file_content = fs::read_to_string(file_path)
-        .expect("Should have been able to read contents of the file");
+    if let Err(error) = run(config) {
+        println!("Application error: {error}");
+        process::exit(1);
+    };
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let file_content = fs::read_to_string(config.file_path)?;
 
     println!("With content:\n\n{file_content}");
+
+    Ok(())
 }
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments")
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Self { query, file_path })
+    }
+}
+
