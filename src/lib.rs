6e1ad6829c8191d5700c70d7f0de0227f7c169b3
Koliras <gyrmanchyk230506@gmail.com>
2024-04-13 20:38:00 +0300

    split code into lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+use std::{error::Error, fs};
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let file_content = fs::read_to_string(config.file_path)?;
+
+    println!("With content:\n\n{file_content}");
+
+    Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments")
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Self { query, file_path })
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::{env, error::Error, fs, process};
+use std::{env, process};
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,35 +13,9 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    if let Err(error) = run(config) {
+    if let Err(error) = minigrep::run(config) {
         println!("Application error: {error}");
         process::exit(1);
     };
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    let file_content = fs::read_to_string(config.file_path)?;
-
-    println!("With content:\n\n{file_content}");
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Self, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments")
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Self { query, file_path })
-    }
 }
 
